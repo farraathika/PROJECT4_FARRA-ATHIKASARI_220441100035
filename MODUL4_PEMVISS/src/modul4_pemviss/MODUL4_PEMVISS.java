c199b04f6f9c58d6c07a38f3c02849019a92c2a6
farraathika <113606626+farraathika@users.noreply.github.com>
2023-10-26 09:34:52 +0700

    Add files via upload

---
+++ MODUL4_PEMVISS/src/modul4_pemviss/MODUL4_PEMVISS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modul4_pemviss;
+
+/**
+ *
+ * @author asus
+ */
+public class MODUL4_PEMVISS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
